ff5571207da975719689b75c6a885d6bc4de8a88
HENRY <113705213+Henrytos@users.noreply.github.com>
2023-10-12 13:00:13 -0300

    estudos

--- todolist/src/main/java/br/com/henry/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/henry/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 //controlador tipo REST
 @RestController
 //ROTA
@@ -22,9 +24,14 @@ public class UserController {
   public ResponseEntity create(@RequestBody UserModel userModel) {
     var user = this.userRepository.findByUsername(userModel.getUsername());
     if (user != null) {
-
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario " + userModel.getUsername() + " existente");
     }
+    //criptografando senha
+    var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+    //alterando senha para criptografada
+    userModel.setPassword(passwordHashred);
+
     var userCreated = userRepository.save(userModel);
     return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
